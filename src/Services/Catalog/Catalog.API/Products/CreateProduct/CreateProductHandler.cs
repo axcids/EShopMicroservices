2475372b813180196752c548aa9dd73c41836a5b
axcids <axcids@gmail.com>
2025-05-18 14:11:27 +0300

    update

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct {
+
+    public record CreateProductCommand(
+            string Name, 
+            List<string> Category,
+            string Description,
+            string ImageFile,
+            decimal Price
+    ) : ICommand<CreateProductCommandResult>;
+    public record CreateProductCommandResult(Guid Id);
+    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductCommandResult> {
+        public Task<CreateProductCommandResult> Handle(CreateProductCommand command, CancellationToken cancellationToken) {
+
+            //Create product entity from command incoming object   
+            var product = new Product {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+            //Save product entity to database   
+
+            //Return the result of the operation  
+            return Task.FromResult(new CreateProductCommandResult(Guid.NewGuid()));
+        }
+    }
+}
