386baba3a298c5c636e1fbc1318d2b3ff2b44cd9
axcids <axcids@gmail.com>
2025-05-22 07:20:25 +0300

    update

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,4 +1,28 @@
-﻿namespace Catalog.API.Products.CreateProduct {
-    public class CreateProductEndpoint {
+﻿namespace Catalog.API.Products.CreateProduct;
+public record CreateProductRequest(
+        string Name,
+        List<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price
+);
+public record CreateProductResponse(Guid Id);
+public class CreateProductEndpoint : ICarterModule {
+    public void AddRoutes(IEndpointRouteBuilder app) {
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) => {
+            // Adapt the new variable "command" from request object
+            var command = request.Adapt<CreateProductCommand>();
+            // Send the command and save the result in a new variable 
+            var result = await sender.Send(command);
+            // Adapt the new variable "response" from the result object
+            var response = result.Adapt<CreateProductResponse>();
+            // Return the response object to the client 
+            return Results.Created($"/products/{response.Id}", response);
+        })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .Produces(StatusCodes.Status400BadRequest)
+            .WithSummary("Create a new product")
+            .WithDescription("Create a new product in the catalog");
     }
 }
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,32 +1,30 @@
 ﻿using BuildingBlocks.CQRS;
 using Catalog.API.Models;
-using MediatR;
 
-namespace Catalog.API.Products.CreateProduct {
+namespace Catalog.API.Products.CreateProduct; 
 
-    public record CreateProductCommand(
-            string Name, 
-            List<string> Category,
-            string Description,
-            string ImageFile,
-            decimal Price
-    ) : ICommand<CreateProductCommandResult>;
-    public record CreateProductCommandResult(Guid Id);
-    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductCommandResult> {
-        public Task<CreateProductCommandResult> Handle(CreateProductCommand command, CancellationToken cancellationToken) {
+public record CreateProductCommand(
+        string Name, 
+        List<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price
+) : ICommand<CreateProductCommandResult>;
+public record CreateProductCommandResult(Guid Id);
+internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductCommandResult> {
+    public async Task<CreateProductCommandResult> Handle(CreateProductCommand command, CancellationToken cancellationToken) {
 
-            //Create product entity from command incoming object   
-            var product = new Product {
-                Name = command.Name,
-                Category = command.Category,
-                Description = command.Description,
-                ImageFile = command.ImageFile,
-                Price = command.Price
-            };
-            //Save product entity to database   
+        //Create product entity from command incoming object   
+        var product = new Product {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+        //  TODO : Save product entity to database   
 
-            //Return the result of the operation  
-            return Task.FromResult(new CreateProductCommandResult(Guid.NewGuid()));
-        }
+        //Return the result of the operation  
+        return new CreateProductCommandResult(Guid.NewGuid());
     }
 }
